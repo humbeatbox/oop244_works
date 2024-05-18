d2bfce9325aa828d0b0833befa3454e2f60a6828
GaryChang <garychang1214@gmail.com>
2024-05-18 12:05:18 -0400

    Add the w1 reflection, edit .gitignore and create the ws2 add the .idea/ file to the ignore list and create the file for ws2

---
+++ ws2/File.cpp
@@ -0,0 +1,8 @@
+//Name:Hsiao-Kang Chang 
+//Seneca email:hchang67@myseneca.ca
+//Seneca Student ID:120049234
+//2024-05-17 Creat this file
+//Done on
+
+
+#include "File.h"
---
+++ ws2/File.h
@@ -0,0 +1,10 @@
+//Name:Hsiao-Kang Chang 
+//Seneca email:hchang67@myseneca.ca
+//Seneca Student ID:120049234
+//2024-05-17 Creat this file
+//Done on
+
+#ifndef WS2_FILE_H
+#define WS2_FILE_H
+
+#endif //WS2_FILE_H
