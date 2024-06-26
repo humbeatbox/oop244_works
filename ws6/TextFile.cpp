c93ef1dd64d011733b982a8725ed0f54bc4fab08
GaryChang <garychang1214@gmail.com>
2024-06-25 23:12:38 -0400

    create the setNoLines function

--- ws6/TextFile.cpp
+++ ws6/TextFile.cpp
@@ -24,6 +24,86 @@ namespace seneca {
     Line::~Line() {
         delete[] m_value;
     }
+    //the below is for TextFile
 
+    //deletes the m_textLines dynamic array and sets is to nullptr deletes the m_filename dynamic Cstring and sets is to nullptr sets m_noOfLines attribute to zero.
+    void TextFile::setEmpty(){
+        delete[] m_textLines;
+        m_textLines = nullptr;
+        delete[] m_filename;
+        m_filename = nullptr;
+        m_noOfLines = 0;
+    }
+    //If the isCopy argument is false, dynamically allocates a Cstring in m_filename and copies the content of the fname argument into it.
+    //If the isCopy argument is true, dynamically allocates a Cstring in m_filename and copies the content of the fname argument with a prefix of "C_" attached to it.
+    void TextFile::setFilename(const char* fname, bool isCopy){
+        if(isCopy){
+            m_filename = new char[strlen(fname) + 3];
+            strcpy(m_filename, "C_");
+            strcat(m_filename, fname);
+        } else{
+            m_filename = new char[strlen(fname) + 1];
+            strcpy(m_filename, fname);
+        }
+    }
+    //Creates a local ifstream object to open the file with the name held in m_filename.
+    //Then it will read the file, character by character, and accumulates the number of newlines in the m_noOfLines attribute.
+    //In the end, it will increase m_noOfLines by one, just in case, the last line does not have a new line at the end.
+    //If the number of lines is zero, it will delete the m_filename and set it to nullptr. (Setting the TextFile to a safe empty state)
+    void TextFile::setNoOfLines(){
+        ifstream f_file(m_filename);
+        if(f_file.is_open()){
+            m_noOfLines = 0;//need the init?
+            string line;//tmp string
+            if(getline(f_file, line)) {// not zero line
+                while (getline(f_file, line)) {//when return is true
+                    m_noOfLines++;
+                }
+                //TODO:check the getline() function is something wrong use the getchar
+                m_noOfLines++;//in case
+            }else{//zero line
+                delete[] m_filename;
+                m_filename = nullptr;
+            }
+        }
+        /*else{
+            delete[] m_filename;
+            m_filename = nullptr;
+        }*/
+
+    }
+    void TextFile::loadText(){
+        if(m_filename){
+            loadText()
+        }
+    }
+    void TextFile::saveAs(const char *fileName)const{
+
+    }
+
+    TextFile& TextFile::operator=(const TextFile&){
+        return *this;
+    }
+    TextFile::~TextFile(){
+
+    }
+    std::ostream& TextFile::view(std::ostream& ostr)const{
+        return ostr;
+    }
+    std::istream& TextFile::getFile(std::istream& istr){
+        return istr;
+    }
+    TextFile::operator bool()const{
+
+    }
+    unsigned TextFile::lines()const{
+
+    }
+    const char* TextFile::name()const{
+
+    }
+    const char* TextFile::operator[](unsigned index)const{
+
+    }
 
 }
--- ws6/TextFile.h
+++ ws6/TextFile.h
@@ -11,26 +11,38 @@
 #include <iostream>
 namespace seneca {
     class Line {
+        //holds the address of the dynamically allocated Cstring (to hold a line of the text file)
         char* m_value{ nullptr };
+        //Returns the address held in the m_value attribute.
         operator const char* ()const;
-        Line() {}
+        //Dynamically allocates memory in m_value and copies the Cstring pointed by lineContent into it.
         Line& operator=(const char*);
+        //Initializes the m_value attribute to nullptr.
+        Line() {}
+        //Makes sure all the allocated memory is freed.
         ~Line();
+
         friend class TextFile;
         // copy and copy assignment prevention goes here
 
     };
 
     class TextFile {
+        //A pointer to hold the dynamic array of Lines. This attribute should be initialized to nullptr
         Line* m_textLines;
+        //A pointer to hold the dynamic Cstring holding the name of the file. This attribute should be initialized to nullptr
         char* m_filename;
+        //An unsigned integer to be set to the number of lines in the file.
         unsigned m_noOfLines;
+        //The page size is the number of lines that should be displayed on the screen before the display is paused.
+        //After these lines are displayed, the user must hit enter for the next page to appear.
         unsigned m_pageSize;
+        void setEmpty();
         void setFilename(const char* fname, bool isCopy = false);
         void setNoOfLines();
         void loadText();
         void saveAs(const char *fileName)const;
-        void setEmpty();
+
     public:
         TextFile(unsigned pageSize = 15);
         TextFile(const char* filename, unsigned pageSize = 15);
