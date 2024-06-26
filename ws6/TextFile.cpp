ea0938faee49a51d982b8635b6ae2ef014263154
GaryChang <garychang1214@gmail.com>
2024-06-26 15:14:12 -0400

    add the constructor and the rest method

--- ws6/TextFile.cpp
+++ ws6/TextFile.cpp
@@ -55,14 +55,15 @@ namespace seneca {
         fin.open(m_filename);
         if(fin.is_open()){
             m_noOfLines = 0;//need the init?
-            string line;//tmp string
-            if(getline(fin, line)) {// not zero line
-                while (getline(fin, line)) {//when return is true
+            //string line;//tmp string
+            char ch;
+            while (fin.get(ch)){
+                if(ch == '\n'){
                     m_noOfLines++;
                 }
-                //TODO:check the getline() function is something wrong use the getchar
-                m_noOfLines++;//in case
-            }else{//zero line
+            }
+            m_noOfLines++;//in case last line no \n
+            if(m_noOfLines == 0){
                 delete[] m_filename;
                 m_filename = nullptr;
             }
@@ -80,6 +81,7 @@ namespace seneca {
 
         }
     }
+    //Saves the content of the TextFile under a new name.
     void TextFile::saveAs(const char *fileName)const{
         ofstream f(fileName);
         if(f.is_open()){
@@ -89,43 +91,111 @@ namespace seneca {
         }
 
     }
-    TextFile::TextFile(unsigned pageSize){
-
+    TextFile::TextFile(unsigned pageSize):m_pageSize(pageSize){
+//        m_pageSize = pageSize;
+        setEmpty();
+//        m_filename = nullptr;
+//        m_noOfLines = 0;
+//        m_textLines = nullptr;
     }
-    TextFile::TextFile(const char* filename, unsigned pageSize){
-
+    //Then if the filename is not null, it will set the filename, set the number of Lines and load the Text (using the corresponding private methods.)
+    TextFile::TextFile(const char* filename, unsigned pageSize):m_pageSize(pageSize){
+//        m_pageSize = pageSize;
+        if(m_filename){
+            setFilename(filename);
+            setNoOfLines();
+            loadText();
+        }else{
+            setEmpty();
+//            m_filename = nullptr;
+//            m_noOfLines = 0;
+//            m_textLines = nullptr;
+        }
     }
-    TextFile::TextFile(const TextFile&){
-
+/*  Initializes the m_pageSize attribute using the m_pageSize of the incoming TextFile object and all the other attributes to nullptr and zero.
+    If the incoming Text object is in a valid State, performs the following tasks to copy the textfile and the content safely:
+    Sets the file-name to the name of the incoming TextFile object (isCopy set to true) See setFilename()
+    Saves the content of the incoming TextFile under the file name of the current TextFile
+    set the number of lines loads the Text*/
+    TextFile::TextFile(const TextFile& right):m_pageSize(right.m_pageSize){
+//        m_pageSize = right.m_pageSize;
+        if(right.m_filename){
+            //TODO:check my step and function call is correct or not
+            setFilename(right.m_filename,true);
+            //create a new file and save into new file
+            right.saveAs(m_filename);
+            setNoOfLines();
+            loadText();
+        }else{
+            setEmpty();
+//            m_filename = nullptr;
+//            m_noOfLines = 0;
+//            m_textLines = nullptr;
+        }
     }
 
-
-    TextFile& TextFile::operator=(const TextFile&){
+    //If the current and the incoming TextFiles are valid
+    //it will first delete the current text and then overwrites the current file and data by the content of the incoming TextFile.
+    TextFile& TextFile::operator=(const TextFile& src){
+        if(this != &src && *this && src){//self-assignment check and not empty state
+            //TODO:check do i need to check the state?
+            delete[] m_textLines;
+            setEmpty();
+            src.saveAs(m_filename);
+            setNoOfLines();
+            loadText();
+        }
         return *this;
     }
     TextFile::~TextFile(){
-
+        delete[] m_textLines;
+        delete[] m_filename;
     }
     std::ostream& TextFile::view(std::ostream& ostr)const{
+        if(m_filename) {
+            cout << m_filename << "\n==========";
+            for (int i = 0; i < m_noOfLines; ++i) {
+                cout << m_textLines[i] << endl;
+                if (i == m_pageSize) {//TODO:check the logic here for next page
+                    cout << "Hit ENTER to continue...\n";
+                    cin.ignore();//TODO: check if no more instruction
+                }
+            }
+        }
         return ostr;
     }
+    //Receives a filename from istr to set the file name of the TextFile. Then sets the number of lines and loads the Text. When done it will return the istr;
     std::istream& TextFile::getFile(std::istream& istr){
+        char* myName = nullptr;
+        cin >> myName;
+        //TODO:check can i use like this?
+        setFilename(myName);
+        setNoOfLines();
+        loadText();
         return istr;
     }
     TextFile::operator bool()const{
-        return *this;
+        return m_filename;
+        //TODO:check the m_filename can present the empty state or not
     }
     unsigned TextFile::lines()const{
-        unsigned ret;
-        return ret;
+        return m_noOfLines;
     }
     const char* TextFile::name()const{
-        char* ret;
-        return ret;
+        return m_filename;
     }
+    //Returns the element in the m_textLine array corresponding to the index argument.
+    //If the TextFile is in an empty state, it will return null. If the index exceeds the size of the array it should loop back to the beginning.
+    //For example, if the number of lines is 10, the last index should be 9 and index 10 should return the first element and index 11 should return the second element.
     const char* TextFile::operator[](unsigned index)const{
-        char* ret;
-        return ret;
+        return (m_textLines && index < m_noOfLines) ? m_textLines[index % m_noOfLines] : nullptr;
+    }
+
+    std::ostream& operator<<(std::ostream& ostr, const TextFile& text){
+        return text.view(ostr);
+    }
+    std::istream& operator>>(std::istream& istr, TextFile& text){
+        return text.getFile(istr);
     }
 
 }
