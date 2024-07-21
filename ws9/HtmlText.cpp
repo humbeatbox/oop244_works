b23658b120fd99e035ddafa3130f340da4fe13aa
GaryChang <garychang1214@gmail.com>
2024-07-21 14:37:17 -0400

    fix(Text&HtmlText): add the #include <cstring>

--- ws9/HtmlText.cpp
+++ ws9/HtmlText.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include "HtmlText.h"
 
 using namespace std;
--- ws9/Text.cpp
+++ ws9/Text.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <cstring>
 #include "Text.h"
 using namespace std;
 namespace seneca {
