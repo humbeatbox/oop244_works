7e978bb39c125960b24d45eadf7a8f6b770d6de9
GaryChang <garychang1214@gmail.com>
2024-07-28 17:18:17 -0400

    feat(main): edit the main

--- ws10/searchNlist.h
+++ ws10/searchNlist.h
@@ -6,7 +6,7 @@
 using namespace std;
 namespace seneca {
     template <typename T1,typename T2>
-    bool search(Collection<T1>& C,Collection<T1> arr[],int arr_size,T2 key){
+    bool search(Collection<T1>& C,const T1* arr,int arr_size,T2 key){
         bool ret{};
         for (int i = 0; i < arr_size; ++i) {
             if(arr[i] == key){
@@ -17,7 +17,7 @@ namespace seneca {
         return ret;
     }
     template <typename T>
-    void listArrayElements(const char* title,Collection<T> arr[],int arr_size){
+    void listArrayElements(const char* title,const T* arr,int arr_size){
         cout << title << endl;
         for (int i = 0; i < arr_size; ++i) {
             cout << i+1 << ": ";
@@ -26,25 +26,6 @@ namespace seneca {
 
 
     }
-//    template <typename T1, typename T2>
-//    bool search(Collection<T1>& collection, const T1* array, int size, const T2& key) {
-//        bool found = false;
-//        for (int i = 0; i < size; i++) {
-//            if (array[i] == key) {
-//                collection.add(array[i]);
-//                found = true;
-//            }
-//        }
-//        return found;
-//    }
-//
-//    template <typename T>
-//    void listArrayElements(const char* title, const T* array, int size) {
-//        std::cout << title << std::endl;
-//        for (int i = 0; i < size; i++) {
-//            std::cout << (i + 1) << ": " << array[i] << std::endl;
-//        }
-//    }
 }
 
 #endif // !SENECA_SEARCHNLIST_H_
