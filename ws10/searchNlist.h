4894d73d7c66efbe717312c315132d36ff613beb
GaryChang <garychang1214@gmail.com>
2024-08-03 18:36:58 -0400

    docs(reflect): add the reflection.

--- ws10/searchNlist.h
+++ ws10/searchNlist.h
@@ -6,8 +6,9 @@
 using namespace std;
 namespace seneca {
     template <typename T1,typename T2>
-    bool search(Collection<T1>& C,const T1* arr,int arr_size,T2 key){
+    bool search(Collection<T1>& C,const T1* arr,const int arr_size,T2 key){
         bool ret{};
+
         for (int i = 0; i < arr_size; ++i) {
             if(arr[i] == key){
                 C.add(arr[i]);
@@ -17,14 +18,12 @@ namespace seneca {
         return ret;
     }
     template <typename T>
-    void listArrayElements(const char* title,const T* arr,int arr_size){
+    void listArrayElements(const char* title,const T* arr,const int arr_size){
         cout << title << endl;
         for (int i = 0; i < arr_size; ++i) {
             cout << i+1 << ": ";
             cout << arr[i] << endl;
         }
-
-
     }
 }
 
