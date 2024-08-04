1363d10b0122643ac4f91b8bdd615fbcd1f8560f
GaryChang <garychang1214@gmail.com>
2024-08-03 22:45:41 -0400

    docs(reflect): add the comment and restriction for template

--- ws10/searchNlist.h
+++ ws10/searchNlist.h
@@ -5,6 +5,17 @@
 
 using namespace std;
 namespace seneca {
+
+    /*
+     * In this template it will search T1 and compare result will add into to Collection.
+     * @tparam T1 Type of the elements in the array must need support comparison (==) with type `T2`, otherwise it will break in compile time.
+     * @tparam T2 Type of the elements ust need support comparison (==) with type `T1`.
+     * @param T1 array should support T2 type (in the array)
+     * @param arr should point to the T1 type array
+     * @param arr_size should input the size of array
+     * @param key should be in the T1 array
+     * @return if search result over 0, it will return true else return false.
+     * */
     template <typename T1,typename T2>
     bool search(Collection<T1>& C,const T1* arr,const int arr_size,T2 key){
         bool ret{};
@@ -17,6 +28,13 @@ namespace seneca {
         }
         return ret;
     }
+
+    /*
+     * This function will print the elements in the array arr
+     * @param title should be the c type string
+     * @param arr point to the T type array
+     * @param arr_size Size of the T type array
+     * */
     template <typename T>
     void listArrayElements(const char* title,const T* arr,const int arr_size){
         cout << title << endl;
